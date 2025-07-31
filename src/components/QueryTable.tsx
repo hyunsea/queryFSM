3802ea46c3088e02cf5a417f07a09fd54b2ccf31
hyunsea <alexhyunsea@gmail.com>
2025-07-31 10:39:02 +0900

    Fix PartIdFilter import error

--- src/components/QueryTable.tsx
+++ src/components/QueryTable.tsx
@@ -3,7 +3,7 @@ import { RotateCcw, Eye, ExternalLink } from 'lucide-react';
 import { QueryJob } from '../utils/mockApi';
 import StatusBadge from './StatusBadge';
 import ProgressBar from './ProgressBar';
-import PartIdFilter from './PartIdFilter';
+import PartIdFilter from './ProcessIdFilter';
 import { formatDateTimeKST } from '../utils/dateUtils';
 
 interface QueryTableProps {
